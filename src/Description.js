bf43fc6dd1484f928d1e959984a1d428219b0fea
selistym <selistym@protonmail.com>
2019-08-20 02:10:06 -0700

    scheduler with latest version

--- src/Description.js
+++ src/Description.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { CONCURRENT_AND_SCHEDULED } from './index'
+import './vendor/fpsmeter.min'
 
-export default function Description() {
+export default class Description extends React.Component {
+  fpsRef = React.createRef();
+
+  componentDidMount() {
+    const anchor = this.fpsRef.current;
+
+    // eslint-disable-next-line no-undef
+    const meter = new FPSMeter(anchor, {
+      heat: true,
+      graph: true
+    });
+
+    function tick() {
+      meter.tick();
+      requestAnimationFrame(tick);
+    }
+
+    tick();
+  }
+
+  render() {
     return (
-        <div className="marketing">
-            <span>Concurrent and Scheduled</span>
-            <div className="spacer" />
-        </div>
+      <div className="marketing">
+        <span>
+          {CONCURRENT_AND_SCHEDULED
+            ? "Concurrent and Scheduled"
+            : "Synchronous"}
+        </span>
+        <div className="spacer" />
+        <div ref={this.fpsRef} className="fps" />
+      </div>
     );
-
+  }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,43 +1,48 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 const SPEED = 0.01;
 
-export default function Header(props) {
-    const timeNow = Date.now();
-    const headerRef = useRef();
-    let frame;
+export default class Header extends React.PureComponent {
+  state = { offset: 0 };
+  t0 = Date.now();
+  ref = React.createRef();
 
-    const animate = () => {
-        requestAnimationFrame(animate);
+  componentDidMount() {
+    this.frame = requestAnimationFrame(this.animate);
+  }
 
-        const text = props.children;
-        const td = Date.now() - timeNow;
-        const offset = td * SPEED;
+  componentWillUnmount() {
+    if (this.frame) {
+      cancelAnimationFrame(this.frame);
+    }
+  }
 
-        const h1 = headerRef.current;
+  animate = () => {
+    requestAnimationFrame(this.animate);
 
-        if (!h1) {
-            return;
-        }
+    const text = this.props.children;
 
-        headerRef.current.innerHTML = "";
+    const td = Date.now() - this.t0;
+    const offset = td * SPEED;
 
-        for (let i = 0; i < text.length; i++) {
-            let charElem = document.createElement("span");
-            charElem.style.color =
-                "hsl(" + (360 * (i + offset)) / text.length + ",80%,60%)";
-            charElem.innerHTML = text[i];
-            h1.appendChild(charElem);
-        }
-    };
+    const h1 = this.ref.current;
 
-    useEffect(() => {
-        frame = requestAnimationFrame(animate);
-        return () => {
-            cancelAnimationFrame(frame);
-        }
-    }, []);
+    if (!h1) {
+      return;
+    }
 
-    
-    return <h1 ref={headerRef} />;// eslint-disable-line
+    h1.innerHTML = "";
+
+    for (let i = 0; i < text.length; i++) {
+      let charElem = document.createElement("span");
+      charElem.style.color =
+        "hsl(" + (360 * (i + offset)) / text.length + ",80%,60%)";
+      charElem.innerHTML = text[i];
+      h1.appendChild(charElem);
+    }
+  };
+
+  render() {
+    return <h1 ref={this.ref} />;
+  }
 }
